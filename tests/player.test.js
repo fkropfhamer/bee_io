55e53ec679880e9fd5eaeb86c388109eb87d9a66
fkropfhamer <fabian.kropfhamer@gmail.com>
2019-11-05 17:15:21 +0100

    added tests

--- tests/player.test.js
+++ tests/player.test.js
@@ -11,3 +11,57 @@ test('BulletHit', () => {
   expect(testPlayer.checkBulletHit(100, 100, 1)).toBe(true);
   expect(testPlayer.checkBulletHit(1000, 1000, 1)).toBe(false);
 });
+
+test('constructor', () => {
+  const mockSocket = { on: jest.fn(() => true) };
+  const mockGame = {};
+  const testPlayer = new Player(mockSocket, mockGame);
+
+  expect(testPlayer.socket).toBe(mockSocket);
+  expect(testPlayer.game).toBe(mockGame);
+  expect(mockSocket.on.mock.calls.length).toBe(3);
+});
+
+test('shoot', () => {
+  const mockSocket = { on: jest.fn(() => true) };
+  const mockGame = { createBullet: jest.fn(() => true) };
+  const testPlayer = new Player(mockSocket, mockGame);
+
+  testPlayer.shoot();
+
+  expect(mockGame.createBullet.mock.calls.length).toBe(1);
+});
+describe('update', () => {
+  test('update position out of bound', () => {
+    const mockSocket = { on: jest.fn(() => true) };
+    const mockGame = { createBullet: jest.fn(() => true) };
+    const testPlayer = new Player(mockSocket, mockGame);
+
+    testPlayer.x = 0;
+    testPlayer.y = 0;
+    testPlayer.angle = 0;
+    testPlayer.speed = 0;
+
+    testPlayer.update();
+
+    expect(testPlayer.x).toBe(684);
+    expect(testPlayer.y).toBe(672);
+    expect(testPlayer.angle).toBe(0);
+    expect(testPlayer.speed).toBe(0);
+  });
+  test('update normal', () => {
+    const mockSocket = { on: jest.fn(() => true) };
+    const mockGame = { createBullet: jest.fn(() => true) };
+    const testPlayer = new Player(mockSocket, mockGame);
+
+    testPlayer.x = 2000;
+    testPlayer.y = 2000;
+    testPlayer.angle = 0;
+    testPlayer.speed = 10;
+
+    testPlayer.update();
+
+    expect(testPlayer.x).toBe(2010);
+    expect(testPlayer.y).toBe(2000);
+  });
+});
